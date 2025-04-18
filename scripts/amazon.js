fd76f4372b68f92ab996475d8161b797954c4879
AshutoshRode <arode1999@gmail.com>
2025-04-18 08:58:10 +0530

    use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,25 +1,4 @@
-// const products = [
-//     {
-//         image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-//         name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//         rating:{
-//             stars: 4.5,
-//             count: 87
-//         },
-//         priceCents: 1090
-//     },{
-//         image:'images/products/intermediate-composite-basketball.jpg',
-//         name:  'Intermediate Size Basketball',
-//         rating:{
-//             stars: 4,
-//             count: 127
-//         },
-//         priceCents:2095
-//     }
-
-
-
-// ];
+import {cart} from '../data/cart.js';
 
 let productsHTML = '';
 
@@ -48,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -64,7 +43,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -79,10 +58,12 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+      const {productId} = button.dataset;
 
       let matchingItem;
 
@@ -91,14 +72,20 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem = item;
 
         }
+    
       });
+      const quantitySelector =
+      document.querySelector(`.js-quantity-selector-${productId}`);
+
+      const quantity = Number(quantitySelector.value);
+      
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: 1
+          productId,
+          quantity
         });
       }
       let cartQuantity =0;
@@ -107,10 +94,30 @@ document.querySelectorAll('.js-add-to-cart')
         
       });
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-      console.log(cartQuantity);
-      console.log(cart);
+
+      const  addedMessage =
+      document.querySelector(
+        `.js-added-to-cart-${productId}` 
+      );
+      addedMessage.classList.add('added-to-cart-visible');
+     
+     
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if(previousTimeoutId){
+        clearTimeout(previousTimeoutId);
+
+      }
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+
+      },2000);
+
+      addedMessageTimeouts[productId]=timeoutId;
+      })
+
+      
     })
-  })
+  
 
 
 
